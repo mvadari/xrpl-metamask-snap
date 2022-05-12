126cce6e28b8c8294547fa3bf568df15a601fd01
Mayukha Vadari <mvadari@ripple.com>
2022-05-12 16:06:02 -0400

    Clean up undesired MetaMask GitHub files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+async function getFees() {
+  const response = await fetch('https://www.etherchain.org/api/gasPriceOracle');
+  return response.text();
+} 
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
+      const fees = await getFees()
       return wallet.request({
         method: 'snap_confirm',
         params: [
@@ -9,7 +15,7 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
             description:
               'This custom confirmation is just for display purposes.',
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              'Current fee estimates: '+fees
           },
         ],
       });
