25c5698a37c5ebc95f4bf6f99c1b8f2a04bcf003
Mayukha Vadari <mvadari@ripple.com>
2022-05-16 17:26:14 -0400

    get JSON RPC working (with CORS proxy)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-async function getFees() {
-  const response = await fetch('https://www.etherchain.org/api/gasPriceOracle');
-  return response.text();
-} 
+// const RIPPLED_URL = 'https://www.etherchain.org/api/gasPriceOracle'
+const RIPPLED_URL = 'https://cors-anywhere.herokuapp.com/https://s2.ripple.com:51234';
+
+async function getRippledData() {
+  try {
+    const response = await fetch(RIPPLED_URL, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({ method: 'server_info' }),
+    });
+    // const response = await fetch(RIPPLED_URL, {method: 'GET', mode: 'cors'});
+    return response.text();
+  } catch (err) {
+    return err.stack;
+  }
+}
 
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
-      const fees = await getFees()
+      const data = await getRippledData();
       return wallet.request({
         method: 'snap_confirm',
         params: [
@@ -14,8 +26,7 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
             prompt: `Hello, ${originString}!`,
             description:
               'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'Current fee estimates: '+fees
+            textAreaContent: `Current fee estimates: ${data}`,
           },
         ],
       });
